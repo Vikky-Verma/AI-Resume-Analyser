53bbe82c95ccd071db2258ddb22f2a2fe5854bb7
Vikky Verma <Vikkyverma7054@gmail.com>
2026-07-09 13:28:42 +0530

    added landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -29,6 +30,7 @@ function App() {
         />
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/welcome" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route

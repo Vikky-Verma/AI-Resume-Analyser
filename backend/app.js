6d1fdaca33e2401c06b4895a71dd47626c75d0d2
Vikky Verma <Vikkyverma7054@gmail.com>
2026-08-08 18:45:59 +0530

    fixed cors error on new deployment

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ app.set("trust proxy", 1);
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://ai-resume-analyser-chi-ten.vercel.app",
+    "https://algo-verse-nu-cyan.vercel.app",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
